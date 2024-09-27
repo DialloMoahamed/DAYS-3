de1fd8495e861c5d24d5146a191419235620273f
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-09-27 01:11:18 +0100

    exercice terminer:Créez un format d'heure lisible par l'homme à l'aide de l'objet Date/Heure. L'heure et la minute doivent toujours être composées de deux chiffres (7 heures doivent être 07 et 5 minutes doivent être 05). AAAA-MM-JJ HH:mm ex. 20120-01-02 07:05

---
+++ Exo3.js
@@ -0,0 +1,11 @@
+const now = new Date();
+
+const year = now.getFullYear();
+const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Ajout de 1 car les mois commencent à 0
+const day = now.getDate().toString().padStart(2, '0');
+const hours = now.getHours().toString().padStart(2, '0');
+const minutes = now.getMinutes().toString().padStart(2, '0');
+
+const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}`;
+
+console.log(formattedDate); // Par exemple : 2023-11-23 15:32
